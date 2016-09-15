86bc0819e6683fd34e1aa8d39edd08174ad1a539
EMMA-TRIUMF <mw720@york.ac.uk>
2016-09-14 18:19:37 -0700

    first commit

---
+++ build/CMakeFiles/exampleB4a.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/exampleB4a.cc" "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/build/CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/src/B4DetectorConstruction.cc" "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4DetectorConstruction.cc.o"
+  "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/src/B4PrimaryGeneratorAction.cc" "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/src/B4RunAction.cc" "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4RunAction.cc.o"
+  "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/src/B4aEventAction.cc" "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4aEventAction.cc.o"
+  "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/src/B4aSteppingAction.cc" "/usr/local/geant/geant4.9.6.p03/examples/basic/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4aSteppingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/geant/geant4.9.6.p03-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
